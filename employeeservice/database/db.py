a947f208609bc994ab7e183ac02cb9bb535591d4
Ashequr Rasul <lovelu@gmail.com>
2026-02-06 01:51:11 +0000

    Kubernetes updated and enviourment variable updated to all the backend

--- employeeservice/database/db.py
+++ employeeservice/database/db.py
@@ -9,14 +9,24 @@ db = None
 def connect_db():
     global client, db
 
-    mongodb_uri = os.getenv("MONGODB_URI")
-    database_name = os.getenv("DATABASE_NAME", "kindergarten")
+    # Read environment variables
+    MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+    MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+    MONGO_DB = os.getenv("MONGO_DB", "kindergarten")
+    MONGO_USER = os.getenv("MONGO_USER")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+    MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE", "admin")
+    mongodb_uri=f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={MONGO_AUTH_SOURCE}"
+
+
+    # mongodb_uri = os.getenv("MONGODB_URI")
+    # database_name = os.getenv("DATABASE_NAME", "kindergarten")
 
     if not mongodb_uri:
         raise RuntimeError("MONGODB_URI is not set")
 
     client = MongoClient(mongodb_uri, serverSelectionTimeoutMS=10000)
-    db = client[database_name]
+    db = client[MONGO_DB]
 
     # Test connection
     client.admin.command("ping")
--- studentservice/database/db.go
+++ studentservice/database/db.go
@@ -15,15 +15,49 @@ var Database *mongo.Database
 
 func Connect() error {
 	// From Environment variables 
-	connectionString := os.Getenv("MONGODB_URI")
+	MONGO_HOST := os.Getenv("MONGO_HOST")
+	if MONGO_HOST == "" {
+		MONGO_HOST = "localhost"
+	}
+	
+	MONGO_PORT := os.Getenv("MONGO_PORT")
+	if MONGO_PORT == "" {
+		MONGO_PORT = "27017"
+	}
+	
+	MONGO_DB := os.Getenv("MONGO_DB")
+	if MONGO_DB == "" {
+		MONGO_DB = "kindergarten"
+	}
+	
+	MONGO_USER := os.Getenv("MONGO_USER")
+	if MONGO_USER == "" {
+		MONGO_USER = "admin"
+	}
+	
+	MONGO_PASSWORD := os.Getenv("MONGO_PASSWORD")
+	if MONGO_PASSWORD == "" {
+		MONGO_PASSWORD = "password123"
+	}
+	
+
+	MONGO_AUTH_SOURCE := os.Getenv("MONGO_AUTH_SOURCE")
+	if MONGO_AUTH_SOURCE == "" {
+		MONGO_AUTH_SOURCE = "admin"
+	}
+
+   // mongodb_uri=f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={MONGO_AUTH_SOURCE}"
+	mongodbURI := "mongodb://" + MONGO_USER + ":" + MONGO_PASSWORD + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DB + "?authSource=" + MONGO_AUTH_SOURCE
+	connectionString := mongodbURI
+	//connectionString := os.Getenv("MONGODB_URI")
 	// if connectionString == "" {
 	// 	connectionString = "mongodb://mongo:27017" // default value
 	// }
 
-	databaseName := os.Getenv("DATABASE_NAME")
-	if databaseName == "" {
-		databaseName = "kindergarten" // default value
-	}
+	// databaseName := os.Getenv("MONGO_DB")
+	// if databaseName == "" {
+	// 	databaseName = "kindergarten" // default value
+	// }
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
